b6708c87c355a0bca8eed4f31d0dfe81c071be3c
clbrunet <brunet.clement5@gmail.com>
2022-04-15 11:57:39 +0200

    refactor: extract drone channels to helpers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -33,7 +33,10 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { Getter } from 'vuex-class';
+
 import PilotsCountInput from '@/components/PilotsCountInput.vue'
+
+import { DJIChannels, sharkbyteChannels, analogChannels } from '@/helpers/channels'
 
 type Pilot = {
   technology: "DJI" | "Sharkbyte" | "Analog";
@@ -41,64 +44,6 @@ type Pilot = {
   channels: { readonly name: string, readonly frequency: number }[];
   channelIndex: number;
 };
-
-const DJIChannels = [
-  { name: "1", frequency: 5660 },
-  { name: "2", frequency: 5695 },
-  { name: "3", frequency: 5735 },
-  { name: "4", frequency: 5770 },
-  { name: "5", frequency: 5805 },
-  // removed channel
-  // { name: "8", frequency: 5839 },
-  { name: "6", frequency: 5878 },
-  { name: "7", frequency: 5914 },
-];
-
-const sharkbyteChannels = [
-  { name: "1", frequency: 5660 },
-  { name: "2", frequency: 5695 },
-  { name: "3", frequency: 5735 },
-  { name: "4", frequency: 5770 },
-  { name: "5", frequency: 5805 },
-  { name: "6", frequency: 5839 },
-  { name: "7", frequency: 5878 },
-  { name: "8", frequency: 5914 },
-];
-
-const analogChannels = [
-  { name: "R1", frequency: 5658 },
-  { name: "R2", frequency: 5695 },
-  { name: "A8", frequency: 5725 },
-  { name: "R3", frequency: 5732 },
-  { name: "B1", frequency: 5733 },
-  { name: "F1", frequency: 5740 },
-  { name: "A7", frequency: 5745 },
-  { name: "B2", frequency: 5752 },
-  { name: "F2", frequency: 5760 },
-  { name: "A6", frequency: 5765 },
-  { name: "R4", frequency: 5769 },
-  { name: "B3", frequency: 5771 },
-  { name: "F3", frequency: 5780 },
-  { name: "A5", frequency: 5785 },
-  { name: "B4", frequency: 5790 },
-  { name: "F4", frequency: 5800 },
-  { name: "A4", frequency: 5805 },
-  { name: "R5", frequency: 5806 },
-  { name: "B5", frequency: 5809 },
-  { name: "F5", frequency: 5820 },
-  { name: "A3", frequency: 5825 },
-  { name: "B6", frequency: 5828 },
-  { name: "F6", frequency: 5840 },
-  { name: "R6", frequency: 5843 },
-  { name: "A2", frequency: 5845 },
-  { name: "B7", frequency: 5847 },
-  { name: "F7", frequency: 5860 },
-  { name: "A1", frequency: 5865 },
-  { name: "B8", frequency: 5866 },
-  { name: "R7", frequency: 5880 },
-  { name: "F8", frequency: 5880 },
-  { name: "R8", frequency: 5917 },
-];
 
 @Component({
   components: {
